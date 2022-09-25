d56e10088f7d3d359273babbe30a83c920211ae4
poojari-prajwal <prajwal.upoojari4@gmail.com>
2022-09-25 12:13:50 +0530

    update

--- chetana_project/settings.py
+++ chetana_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chetana_app',
+    'chetana_app.apps.ChetanaAppConfig',
 ]
 
 MIDDLEWARE = [
