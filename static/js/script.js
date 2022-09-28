3507b676a79cae6fdd633384f172e382543f7e78
poojari-prajwal <prajwal.upoojari4@gmail.com>
2022-09-28 15:39:05 +0530

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const hamburger = document.querySelector('.headers .nav-bars .nav-lists .hamburger');
+const mobile_menu = document.querySelector('.headers .nav-bars .nav-lists ul');
+const menu_item = document.querySelectorAll('.headers .nav-bars .nav-lists ul li a');
+const header = document.querySelector('.headers.containers');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -10,8 +10,8 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#43cae9';
+	if (scroll_position > 50) {
+		header.style.backgroundColor = '#17a2c2';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
